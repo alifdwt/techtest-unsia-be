b1cd799e6ae74a7451b118d7222c5b30c29e1ba0
Alif Dewantara <aputradewantara@outlook.com>
2026-01-29 16:25:42 +0700

    change questions respose key

--- internal/service/dto.go
+++ internal/service/dto.go
@@ -3,14 +3,14 @@ package service
 import "github.com/google/uuid"
 
 type QuestionDTO struct {
-	ID      uuid.UUID
-	Type    string
-	Text    string
-	Points  int32
-	Options []OptionDTO
+	ID      uuid.UUID   `json:"id"`
+	Type    string      `json:"type"`
+	Text    string      `json:"text"`
+	Points  int32       `json:"points"`
+	Options []OptionDTO `json:"options"`
 }
 
 type OptionDTO struct {
-	ID   uuid.UUID
-	Text string
+	ID   uuid.UUID `json:"id"`
+	Text string    `json:"text"`
 }
